1a68c570b30c40c0a62e8d3d321fb0546afb4197
ujvarivera <ujvariveronika0320@gmail.com>
2024-03-23 18:40:31 +0100

    update, delete event api routes

--- app/api/events/[eventId]/route.ts
+++ app/api/events/[eventId]/route.ts
@@ -8,13 +8,43 @@ export async function GET(request: NextRequest, {params: {eventId}}) {
         }
     })
 
+    if (!event) {
+        return NextResponse.json({message: "Event not found"}, {status: 404})
+    }
+
     return NextResponse.json(event);
 }
 
 export async function PUT(request: NextRequest, {params: {eventId}}) {
-    
+    const body = await request.json();
+
+    try {
+        const updatedEvent = await prisma.event.update({
+            where: {
+                id: parseInt(eventId)
+            },
+            data: {
+                title: body.title,
+                description: body.description,
+            }
+        })        
+        return NextResponse.json(updatedEvent);
+
+    } catch (error) {
+        return NextResponse.json({message: "Event not found"});
+    }
 }
 
 export async function DELETE(request: NextRequest, {params: {eventId}}) {
-    
+    try {
+        const deletedEvent = await prisma.event.delete({
+            where: {
+                id: parseInt(eventId)
+            },
+        })
+        return NextResponse.json(deletedEvent);
+        
+    } catch (error) {
+        return NextResponse.json({message: "Event not found"});
+    }
 }
